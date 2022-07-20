a9aa8d23c6acea244084e814a2b46689f2de010a
Lilian Crivelli <liliandevsistem@gmail.com>
2022-07-19 21:40:14 -0400

    Variáveis Tipos de Dados e Oper. Matem. em Java

---
+++ Variáveis, Tipos de Dados e Operadores Matemáticos em Java/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+  /**
+   * @param args
+   */
+  public static void main(String[] args){
+    int i;
+    //int i;
+    int I;
+    //int 1a;
+    int _1a;
+    int $aq;
+
+    i = 5;
+    I = 10;
+    _1a = 20;
+    $aq = 7;
+
+    final int j = 10;
+   // j =15;
+    int asrn24621md;
+    //int asnr246 21md;
+    int asrn24621_md = 10;
+    //int asrn24%621_md = 10;
+
+
+    asrn24621md = 100;
+    asrn24621_md = 10;
+
+int quantidadeProduto = 50;
+//int QuantidadeProduto;
+final int NUMERO_TENTATIVAS = 5;
+//final int numeroTemtativas = 5;
+int QUANTIDADE_OPCOES = 25;
+//int qtdProd;
+
+System.out.println(i);
+System.out.println(I);
+System.out.println(_1a);
+System.out.println($aq);
+
+System.out.println(j);
+System.out.println(asrn24621md);
+System.out.println(asrn24621_md);
+
+System.out.println(quantidadeProduto);
+System.out.println(NUMERO_TENTATIVAS);
+System.out.println(QUANTIDADE_OPCOES);
+
+
+  }
+}
