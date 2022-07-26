a13fba7e2fbca9991f10d7d7df93f8aa353b7ea3
Lilian Crivelli <liliandevsistem@gmail.com>
2022-07-26 00:18:50 -0400

    Aprendizados da aula 02 da dio

---
+++ Exercicios Java/Variáveis, Tipos de Dados e Operadores Matemáticos em Java/com/dio/Aula02.java
@@ -0,0 +1,60 @@
+package com.dio;
+
+public class Aula02 {
+
+        public static void Aula02(String[] args) {
+
+            byte b1 = 10;
+            byte b2 = 20;
+
+            short s1 = 10000;
+//short s2 = 20000;
+
+            int i1 = -100000000;
+            int i2 = 28500;
+
+            long l1 = 1000000000000000000L;
+//long l2 = 200400005000584750000L;
+
+//float f1 = 4.5;
+            float f2 = 10.68F;
+
+            double d1 = 85.69;
+            double d2 = 99.84D;
+
+            char c1 = 'w';
+
+//char c2 = 'Tw';
+
+            char c3 = '\u0057';
+
+            String st1 = "Fulano da Silva";
+            String st2 = "Cicrano de Souza";
+
+//String dt1 = "19/02/1981";
+
+
+            boolean bo1 = true;
+            boolean bo2 = false;
+
+            System.out.println(b1);
+            System.out.println(b2);
+            System.out.println(s1);
+            System.out.println(i2);
+            System.out.println(b1);
+            System.out.println(l1);
+            System.out.println(f2);
+            System.out.println(d1);
+            System.out.println(d2);
+            System.out.println(c1);
+            System.out.println(c3);
+            System.out.println(st1);
+            System.out.println(st2);
+            System.out.println(bo1);
+            System.out.println(bo2);
+
+        }
+
+    }
+
+
